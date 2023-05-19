155f21cd6e883246d01c13f9916f6d091de78ac2
colodev01 <francomuzzin01@gmail.com>
2023-05-19 15:49:09 -0300

    Add fronend folder and navbar component

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+import Dropzone from './components/Dropzone.jsx';
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Dropzone.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+<div>
+    teamu
+</div>
